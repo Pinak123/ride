1082a4e45c4db51bf030bb6772547f3ff4c56db6
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-09-20 16:26:40 +0530

    Navbar completed

---
+++ my-project/src/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import NavbarComp from './components/Navbar'
+
+function Layout() {
+  return (
+    <>
+      <NavbarComp/>
+      <Outlet/>
+    </>
+  )
+}
+
+export default Layout
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Layout from './Layout.jsx'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from './components/DashboardComponent.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      
+      {
+        path: "",
+        element: <Dashboard/>
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </StrictMode>,
 )
